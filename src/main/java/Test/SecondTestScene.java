76a5f376602dccb5a2a7187f058d09a1ef61e9ad
peershaul <peer.shaul1@gmail.com>
2021-04-16 13:13:27 +0300

    Playing with inplumentations of a line drawer in the engine

--- src/main/java/Test/SecondTestScene.java
+++ src/main/java/Test/SecondTestScene.java
@@ -1,26 +1,9 @@
 package Test;
 
 import Scenes.Scene;
-import components.GameObject;
-import components.Sprite;
-import components.Transform;
-import components.shape.Circle;
-import components.shape.Rectangle;
-import components.shape.Shape;
-import graphics.*;
-import org.joml.Random;
-import org.joml.Vector2f;
-import org.joml.Vector3f;
-import org.joml.Vector4f;
-import utils.AssetPool;
-import utils.KeyListener;
-import utils.Window;
 
-
-import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL20.GL_MAX_TEXTURE_IMAGE_UNITS;
-import static utils.Color.normalize;
+import static org.lwjgl.opengl.GL11C.GL_LINES;
 
 public class SecondTestScene extends Scene {
 
@@ -28,116 +11,24 @@ public class SecondTestScene extends Scene {
         super("second test scene");
     }
 
-    private ArrayBuffer arrayBuffer;
-    private VertexBuffer vb;
-    private Shader shader;
-    private GameObject tester, go;
-    private Vector3f multi = new Vector3f(1);
-    private float divider = 1, velocity = 200, time = 10;
-    private Transform circTransform;
-
     @Override
     public void init(){
 
-        addRendererToScene(new Renderer(
-                AssetPool.getShader("resources/shaders/test.glsl"),
-                new int[]{ 2, 4 }
-        ));
+    }
 
-        renderers.get(0).getShader().uploadVec2f("screen", Window.getScreen());
+    @Override
+    public void update(float dt){
+        glBegin(GL_LINES);
+        glVertex2f(0.5f, 0);
+        glVertex2f(-0.5f, 1);
+        glEnd();
 
-        go = new GameObject("Test object",
-                new Transform(
-                        new Vector2f(100),
-                        new Vector2f(500)
-                ));
-
-        Sprite spr = new Sprite(new Rectangle(go.transform));
-        spr.zIndex = 0;
-
-        go.addComponent(spr);
-
-        renderers.get(0).addSprite(go);
-        addGameObjectToScene(go);
-
-        tester = new GameObject(
-                "test 2.0",
-                new Transform(
-                        new Vector2f(-100),
-                        new Vector2f(300)
-                )
-        );
-
-        Sprite anotherSpr = new Sprite(new Rectangle(tester.transform));
-
-        anotherSpr.zIndex = -1;
-
-        tester.addComponent(anotherSpr);
-        renderers.get(0).addSprite(tester);
-        addGameObjectToScene(tester);
-
-        Sprite testSprite = new Sprite(new Rectangle(new Transform(
-                new Vector2f(-200),
-                new Vector2f(300)
-        )));
-
-        testSprite.zIndex = 2;
-        testSprite.color = new Vector4f(0, 1, 0, 0.25f);
-
-        addToRenderer(0, testSprite);
-
-        circTransform = new Transform(new Vector2f(-500, 400));
-        float radius = 200;
-        int resolution = 40;
-        Shape circ = new Circle(radius, resolution, circTransform);
-
-        Sprite circSpr = new Sprite(circ);
-        circSpr.color = normalize(new Vector4f(30, 50, 203, 0.4f));
-
-        addToRenderer(0, circSpr);
+        glEnable(GL_LINE_SMOOTH);
+        glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
     }
 
     @Override
     public void SceneUpdate(float dt) {
-        Random rand = new Random();
 
-        Sprite spr = go.getComponent(Sprite.class);
-
-        Vector4f c = spr.color;
-
-        if(c.x >= 1 || c.x <= 0)
-            multi.x *= -1;
-        if(c.y >= 1 || c.y <=0 )
-            multi.y *= -1;
-        if(c.z >= 1 || c.z <= 0)
-            multi.z *= -1;
-
-        spr.color = new Vector4f(
-                c.x + (multi.x * dt * rand.nextFloat() / divider),
-                c.y + (multi.y * dt * rand.nextFloat() / divider),
-                c.z + (multi.z * dt * rand.nextFloat() / divider),
-                1
-        );
-
-        if(KeyListener.isKeyPressed(GLFW_KEY_W))
-            tester.transform.position.y += velocity * dt;
-
-        else if(KeyListener.isKeyPressed(GLFW_KEY_S))
-            tester.transform.position.y -= velocity * dt;
-
-        if(KeyListener.isKeyPressed(GLFW_KEY_D))
-            tester.transform.position.x += velocity * dt;
-
-        else if(KeyListener.isKeyPressed(GLFW_KEY_A))
-            tester.transform.position.x -= velocity * dt;
-
-        time += dt;
-
-        circTransform.position.x += 10 * dt;
-
-        if(KeyListener.isKeyPressed(GLFW_KEY_L) && time >= 0.25f) {
-            for (Renderer rend : renderers) rend.toggleWireframe();
-            time = 0;
-        }
     }
 }
